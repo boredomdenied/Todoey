7bbf8407d4ffdf27bc281770a137a40c3aeaaef1
boredomdenied <rackstackandrender@gmail.com>
2018-05-23 16:20:33 -0500

    Persisting data using Codable protocol.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Brandon Chapman on 5/22/18.
+//  Copyright © 2018 Brandon Chapman. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable  {
+    var title: String = ""
+    var done: Bool = false
+    ;
+}
